0dddcc5b6a0bdbb972c60c035f095cfa58a9cc1b
Sabirkaddouri <sabirkaddouri152@gmail.com>
2025-09-25 11:03:44 -0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,3 +6,10 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerHTML= `The area of the rectangle is: ${area}`;
 }
+function groceryTracker() {
+    let A1 = parseFloat(document.getElementById('Amount1').value);
+    let A2 = parseFloat(document.getElementById('Amount2').value);
+    let A3 = parseFloat(document.getElementById('Amount3').value);
+    let total = A1+A2+A3;
+    document.getElementById('total').innerHTML = `The total amounts is : ${total} $`;
+}
